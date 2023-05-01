ddc57896233bad53d93d2b93a73f6c1d0435e066
Pelin Balcı <balci.pelin@gmail.com>
2023-05-01 22:44:11 +0300

    streamlit

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ https://www.kaggle.com/datasets/basilb2s/language-detection?resource=download
 
 https://learn.microsoft.com/en-us/windows/wsl/install-manual#step-4---download-the-linux-kernel-update-package
 
+https://stackoverflow.com/questions/5834014/lf-will-be-replaced-by-crlf-in-git-what-is-that-and-is-it-important
 
 ## Build Docker
 docker build -t language-detection-app .
---
+++ app/streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import json
+import requests
+
+st.title("Language Detection Model")
+st.write("")
+title = st.text_input('Input text', 'Please enter the text here')
+st.write('The current text is', title)
+
+inputs = {"text": title}
+
+if st.button("Detect Language"):
+    res = requests.post(url = "http://127.0.0.1:80/predict", data=json.dumps(inputs))
+
+    st.subheader(f"response from API = {res.text}")
